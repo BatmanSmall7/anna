cc0eae487ab1cc3a2a1494ef5b3dc1f2aa1d176a
xwz982298763 <982298763@qq.com>
2018-06-14 17:39:11 +0800

    init git

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+            叔叔若是有心，喝了奴家这半盏残酒
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
